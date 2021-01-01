427bd0a1c18327848e337264527eded39ed284a5
Charlie Bevis <charliebevis@gmail.com>
2021-01-01 11:28:10 -0600

    check if a file exists, we'll use it for caching

--- src/Run.hs
+++ src/Run.hs
@@ -2,8 +2,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Run (run) where
 
-import Import
+
+import           Import
+import           Network.HTTP.Simple
+import           RIO.FilePath        ((</>))
+import           System.Directory
 
 run :: RIO App ()
 run = do
-  logInfo "We're inside the application!"
+    p <- liftIO path
+    exists <- liftIO (doesFileExist p)
+    exitIfExists exists
+
+    -- logInfo $ displayShow exists
+  -- logInfo . L.take 500 =<< httpLBS url
+
+
+exitIfExists :: Bool -> RIO App ()
+exitIfExists exists =
+  if exists then
+      logInfo (displayShow "exists")
+      -- exitSuccess
+  else
+      logInfo $ displayShow "doesn't exist"
+      -- do
+      --   (response <- httpLbs "http://httpbin.org/get"
+      --   logInfo $ displayShow "The body: " <> (displayShow . getResponseBody ) response
+      --   logInfo $ displayShow "The status code was: " <> displayShow (getResponseStatusCode response)
+      --     )      -- print $ getResponseHeader "Content-Type" response
+      -- logInfo $ displayShow $ getResponseBody response
+
+path =
+  do
+    cwd <- getCurrentDirectory
+    return $ cwd ++ "/fetched.html"
