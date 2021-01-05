a5845a9c55150591237de30681514d80fcab4f98
Charlie Bevis <charliebevis@gmail.com>
2021-01-05 07:33:07 -0600

    use XPath to pull the article contents out... but how to get things back into HTML?

--- src/Run.hs
+++ src/Run.hs
@@ -3,16 +3,20 @@
 module Run (run) where
 
 
+import           Data.ByteString.Lazy.Char8 as C
+import           RIO.Text                  as T
 import           Import
 import           Network.HTTP.Simple
 import           System.Directory
-import Data.ByteString.Lazy.Char8 as C
+import           Text.HTML.DOM              (parseLBS)
+import           Text.XML.Cursor
 
 run :: RIO App ()
 run = do
-    cwd <- liftIO getCurrentDirectory
-    let args = makeArg cwd <$> [1..25]
-    traverse_ (uncurry memoizedFetch) args
+  cwd <- liftIO getCurrentDirectory
+  let args = makeArg cwd <$> [1..25]
+  traverse_ (uncurry memoizedFetch) args
+  parseBody $ snd (makeArg cwd 1)
 
 memoizedFetch :: String -> FilePath -> RIO App ()
 memoizedFetch address path = do
@@ -30,7 +34,32 @@ memoizedFetch address path = do
       logInfo $ displayShow "The status code was: " <> displayShow (getResponseStatusCode response)
 
 
-makeArg :: FilePath -> Int -> (String, String)
+makeArg :: FilePath -> Int -> (String, FilePath)
 makeArg cwd i =
   ("https://adventofcode.com/2020/day/" <> show i
   , cwd ++ "/advents/" <> show i <>".html")
+
+parseBody :: FilePath -> RIO App ()
+parseBody filePath =
+    liftIO $ withLazyFile filePath
+      (\file ->
+        do
+          let cursor = cursorFor file
+          -- processData $
+          let foundStuff = cursor $// findNodes &| extractData
+          putStrLn "found nodes:"
+          traverse_ (putStrLn . C.pack . T.unpack) foundStuff
+          return ()
+      )
+  >> return ()
+
+cursorFor :: C.ByteString -> Cursor
+cursorFor file =
+  fromDocument $ parseLBS file
+
+findNodes :: Cursor -> [Cursor]
+findNodes = element "main" &/ element "article"
+
+
+extractData :: Cursor -> Text
+extractData = tshow . node -- T.concat . content
