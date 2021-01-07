e15f9c407e7c35ba42526311170f9fcbb038d181
Charlie Bevis <charliebevis@gmail.com>
2021-01-07 13:56:29 -0600

    compile these files into something readable

---
+++ compact.sh
@@ -0,0 +1,12 @@
+echo "<html>" > compacted1.html
+cat "adventsClean/1.html" >> compacted1.html
+cat "articlesClean/1.html" >> compacted1.html
+cat "adventsClean/2.html" >> compacted1.html
+cat "articlesClean/2.html" >> compacted1.html
+cat "adventsClean/3.html" >> compacted1.html
+cat "articlesClean/3.html" >> compacted1.html
+cat "adventsClean/4.html" >> compacted1.html
+cat "articlesClean/4.html" >> compacted1.html
+cat "adventsClean/5.html" >> compacted1.html
+cat "articlesClean/5.html" >> compacted1.html
+echo "</html>">> compacted1.html
